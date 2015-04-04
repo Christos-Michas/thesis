1958502a7f486a629248c19d157d34a6491f4ba6
Christos-Michas <cmichas3@gmail.com>
2015-04-04 16:57:28 +0300

    Improved comments in set_epochs_param.py

--- set_epochs_param.py
+++ set_epochs_param.py
@@ -1,19 +1,23 @@
 ##goal is to set IDs correctly in IDs matrix
-##ID is set to 1 for acceptable trials, to 2 for rejected trials
+##ID is set to 1 for accepted trials, to 2 for rejected trials
 
 import numpy
 ##get sound durations for all 110 trials
 durations = numpy.genfromtxt('mne_data/thesis/DATA/durations.txt')
 durations = numpy.int32(1000*durations)
 
-##create matrices
-##initial has ids of the 110 trials in the order they appear in the experiment (subject x trial)
+##instantiate matrices##
+
+##initial has for each subject ids of the 110 trials in the order they appear in the experiment (subject x trial)
 initial = numpy.ones((10,110),dtype=int)
+
 ##IDs has IDs in order (subject x sound x trial)
 IDs = numpy.ones((10,11,10),dtype=int)
 
 
-##set rejected ids
+##set rejected ids##
+
+##subject 2
 initial[1,0] = 2
 initial[1,1] = 2
 initial[1,10] = 2
@@ -22,6 +26,7 @@ initial[1,34] = 2
 initial[1,61] = 2
 initial[1,98] = 2
 
+##subject 3
 initial[2,0] = 2
 initial[2,1] = 2
 initial[2,2] = 2
@@ -44,11 +49,15 @@ initial[2,96] = 2
 initial[2,107] = 2
 initial[2,109] = 2
 
+
+##subject 4
 initial[3,0] = 2
 initial[3,29] = 2
 initial[3,36] = 2
 initial[3,80] = 2
 
+
+#subject 5
 initial[4,0] = 2
 initial[4,1] = 2
 initial[4,2] = 2
@@ -65,9 +74,13 @@ initial[4,78] = 2
 initial[4,84] = 2
 initial[4,85] = 2
 
+
+#subject 6
 initial[5,0] = 2
 initial[5,1] = 2
 
+
+##subject 7
 initial[6,0] = 2
 initial[6,1] = 2
 initial[6,2] = 2
@@ -84,10 +97,14 @@ initial[6,51] = 2
 initial[6,60] = 2
 initial[6,83] = 2
 
+
+#subject 10
 initial[9,0] = 2
 initial[9,1] = 2
 initial[9,2] = 2
 
+
+##copy ids from "initial" to "IDs"##
 
 for i in numpy.arange(0,10):
 	trials = numpy.zeros(11,dtype=int)
